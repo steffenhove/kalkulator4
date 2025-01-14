b06ab01b373a2455c8f44afd8d0f24d905a28ae8
steffenhove <steffenhove@gmail.com>
2025-01-14 22:41:39 +0100

    Update HistoryViewModel.kt

--- app/src/main/java/no/steffenhove/betongkalkulator/ui/viewmodel/HistoryViewModel.kt
+++ app/src/main/java/no/steffenhove/betongkalkulator/ui/viewmodel/HistoryViewModel.kt
@@ -1,27 +1,21 @@
 package no.steffenhove.betongkalkulator.ui.viewmodel
 
-import androidx.lifecycle.*
-import kotlinx.coroutines.launch
-import no.steffenhove.betongkalkulator.ui.model.CalculationEntity
-import no.steffenhove.betongkalkulator.ui.repository.CalculationRepository
+// ... (importer beholdes) ...
 
 class HistoryViewModel(private val repository: CalculationRepository) : ViewModel() {
-
-        val allCalculations: LiveData<List<CalculationEntity>> = repository.allCalculations.asLiveData()
+    val allCalculations: Flow<List<CalculationEntity>> = repository.allCalculations
 
     fun insert(calculation: CalculationEntity) = viewModelScope.launch {
         repository.insert(calculation)
     }
 
-    fun delete(calculation: CalculationEntity) = viewModelScope.launch {
-        repository.delete(calculation)
+    fun deleteCalculations(ids: List<Int>) {
+        viewModelScope.launch {
+            repository.deleteCalculations(ids)
+        }
     }
 
     fun deleteAll() = viewModelScope.launch {
         repository.deleteAll()
-    }
-
-    fun calculateTotalWeight(calculations: List<CalculationEntity>): Double {
-        return calculations.sumOf { it.result }
     }
 }
