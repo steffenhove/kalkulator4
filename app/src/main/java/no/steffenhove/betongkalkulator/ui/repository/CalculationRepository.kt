c2a0417513489658d0a7327b04a8123b2f04bba9
steffenhove <steffenhove@gmail.com>
2025-01-14 22:42:05 +0100

    Update CalculationRepository.kt

--- app/src/main/java/no/steffenhove/betongkalkulator/ui/repository/CalculationRepository.kt
+++ app/src/main/java/no/steffenhove/betongkalkulator/ui/repository/CalculationRepository.kt
@@ -1,26 +1,24 @@
 package no.steffenhove.betongkalkulator.ui.repository
 
-import androidx.annotation.WorkerThread
-import kotlinx.coroutines.flow.Flow
 import no.steffenhove.betongkalkulator.ui.model.CalculationDao
 import no.steffenhove.betongkalkulator.ui.model.CalculationEntity
 
 class CalculationRepository(private val calculationDao: CalculationDao) {
+    val allCalculations = calculationDao.getAllCalculations()
 
-    val allCalculations: Flow<List<CalculationEntity>> = calculationDao.getAllCalculations()
-
-    @WorkerThread
     suspend fun insert(calculation: CalculationEntity) {
         calculationDao.insert(calculation)
     }
 
-    @WorkerThread
     suspend fun delete(calculation: CalculationEntity) {
         calculationDao.deleteCalculation(calculation)
     }
 
-    @WorkerThread
     suspend fun deleteAll() {
         calculationDao.deleteAllCalculations()
+    }
+
+    suspend fun deleteCalculations(ids: List<Int>) {
+        calculationDao.deleteCalculations(ids)
     }
 }
