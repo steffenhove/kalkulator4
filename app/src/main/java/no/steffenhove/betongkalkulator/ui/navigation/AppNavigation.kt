6979e044ef3f54cf274490cfc2aee18a44ea9a2a
steffenhove <steffenhove@gmail.com>
2025-01-14 22:42:28 +0100

    Update AppNavigation.kt

--- app/src/main/java/no/steffenhove/betongkalkulator/ui/navigation/AppNavigation.kt
+++ app/src/main/java/no/steffenhove/betongkalkulator/ui/navigation/AppNavigation.kt
@@ -1,21 +1,30 @@
 package no.steffenhove.betongkalkulator.ui.navigation
 
+import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import no.steffenhove.betongkalkulator.ui.screens.CalculationScreen
-import no.steffenhove.betongkalkulator.ui.screens.SettingsScreen
-import no.steffenhove.betongkalkulator.ui.screens.StartScreen
 import no.steffenhove.betongkalkulator.ui.screens.HistoryScreen
-import no.steffenhove.betongkalkulator.ui.utils.getUnitSystemPreference
+import no.steffenhove.betongkalkulator.ui.screens.SettingsScreen
 
 @Composable
-fun AppNavigation(navController: NavHostController) {
-    NavHost(navController, startDestination = "start") {
-        composable("start") { StartScreen(navController) }
-        composable("settings") { SettingsScreen(navController.context) }
-        composable("calculator") { CalculationScreen(getUnitSystemPreference(navController.context), navController.context) }
-        composable("history") { HistoryScreen(navController.context) }
+fun AppNavigation(navController: NavHostController, context: Context, modifier: Modifier = Modifier) {
+    NavHost(navController = navController, startDestination = Screen.Calculation.route, modifier = modifier) {
+        composable(Screen.Calculation.route) {
+            CalculationScreen(context = context)
+        }
+        composable(Screen.History.route) {
+            HistoryScreen(
+                context = context,
+                onNavigateToSettings = { navController.navigate(Screen.Settings.route) },
+                onNavigateToCalculation = { navController.navigate(Screen.Calculation.route) }
+            )
+        }
+        composable(Screen.Settings.route) {
+            SettingsScreen(context = context)
+        }
     }
 }
